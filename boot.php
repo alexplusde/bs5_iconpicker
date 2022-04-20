dda9b84e5645afbf81a0e312edf35e416c9c86ed
Alexander Walther <aw@alexplus.de>
2022-04-20 23:55:43 +0200

    initial commit

--- boot.php
+++ boot.php
@@ -1,25 +1,5 @@
 <?php
-# Die boot.php wird bei jedem Seitenaufruf im Frontend und Backend aufgeführt, je nach Reihenfolge von Abhängigkeiten in der package.yml vor oder nach anderen Addons.
 
-# Beispiel YOrm Model-Klasse registrieren, wenn das Addon mit einer eigenen YForm Tabelle kommt.
-/*
-if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
-rex_yform_manager_dataset::setModelClass(
-    'rex_dummy',
-    dummy::class
-);
-*/
-
-# Prüfen, ob ein anderes Addon installiert ist, bspw. Cronjob-Addon
-/*
-if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
-    rex_cronjob_manager::registerType('rex_cronjob_dummy');
-}
-*/
-
-# Beim Extension Point REX_YFORM_SAVED etwas ausführen
-/*
-rex_extension::register('REX_YFORM_SAVED', function (rex_extension_point $ep) {
-    // Mein Code, oder meine Funktion / statische Methode aufrufen
-});
-*/
+$addon = rex_addon::get('bs5_iconpicker');
+rex_view::addCssFile($addon->getAssetsUrl('/font/bootstrap-icons.css'));
+rex_view::addJsFile($addon->getAssetsUrl('iconpicker.js'));
--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,43 @@
 <?php
+
+?>
+<div class="input-group">
+        <span class="input-group-addon selected-icon-0"></span>
+    <input type="text" class="form-control iconpicker" />
+</div>
+    <div class="input-group">
+            <span class="input-group-addon selected-icon-1"></span>
+        <input type="text" class="form-control iconpicker" />
+    </div>
+    <div class="input-group">
+            <span class="input-group-addon selected-icon-2"></span>
+        <input type="text" class="form-control iconpicker" />
+    </div>
+
+<script>
+    (async () => {
+        const response = await fetch('/assets/addons/bs5_iconpicker/iconsets/bootstrap5.json')
+        const result = await response.json()
+
+
+        var all = document.querySelectorAll('.iconpicker');
+
+        Array.from({length: all.length}, (v, k) => k).forEach((index) => {
+         let element = all.item(index);
+
+            iconpicker = new Iconpicker(element, {
+                icons: result,
+                showSelectedIn: document.querySelector('.selected-icon-'+index),
+                searchable: true,
+                selectedClass: "selected",
+                containerClass: "my-picker",
+                hideOnSelect: true,
+                fade: false,
+                defaultValue: 'bi bi-app',
+                valueFormat: val => `bi ${val}`
+            });
+
+        });
+
+    })()
+</script>
