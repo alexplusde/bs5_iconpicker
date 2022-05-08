697a1771ea3c235b696309003760145fe081f0c7
eaCe <webmaster@slaps.de>
2022-05-08 21:18:23 +0200

    feat: use rex:ready, set default/selected value

--- assets/js/bs5-iconpicker.js
+++ assets/js/bs5-iconpicker.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', async function () {
+$(document).on('rex:ready', async function (event, container) {
     const response = await fetch('/assets/addons/bs5_iconpicker/iconsets/bootstrap5.json');
     const result = await response.json();
 
@@ -9,14 +9,14 @@ window.addEventListener('load', async function () {
         element.classList.add('form-control');
 
         // check if input-group is available
-        if(!element.parentNode.classList.contains('input-group')) {
+        if (!element.parentNode.classList.contains('input-group')) {
             element.parentNode.classList.add('input-group');
             // force display table...
             element.parentNode.style.setProperty('display', 'table', 'important');
         }
 
         // check if input-group-addon is available
-        if(!(element.previousElementSibling && element.previousElementSibling.classList.contains('input-group-addon'))) {
+        if (!(element.previousElementSibling && element.previousElementSibling.classList.contains('input-group-addon'))) {
             //set input-group and default icon
             const placeholder = document.createElement('div');
             placeholder.innerHTML = '<span class="input-group-addon selected-icon-0"><i class="bi bi-app"></i></span>';
@@ -31,7 +31,7 @@ window.addEventListener('load', async function () {
             containerClass: 'bs5-picker',
             hideOnSelect: true,
             fade: false,
-            defaultValue: 'bi-app',
+            defaultValue: element.value || 'bi-app',
             valueFormat: val => `bi ${val}`
         });
 
