7106b52313215d4694e104e586103d1831abfd1e
alxndr-w <alxndr-w@users.noreply.github.com>
2023-12-15 21:54:07 +0000

    Apply php-cs-fixer changes

--- boot.php
+++ boot.php
@@ -4,7 +4,6 @@ if (rex_addon::get('yform')->isAvailable()) {
     rex_yform::addTemplatePath(rex_path::addon('bs5_iconpicker', 'ytemplates'));
 }
 
-
 $addon = rex_addon::get('bs5_iconpicker');
 rex_view::addCssFile($addon->getAssetsUrl('/font/bootstrap-icons.css'));
 rex_view::addCssFile($addon->getAssetsUrl('/styles/bs5-iconpicker.css'));
