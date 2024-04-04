a9d38e46afbf2d3bcfd1a0f2f64e57ea5eaf7c84
Alexander Walther <mail@alexplus.de>
2024-04-04 22:44:35 +0200

    added fa6 support
    closes #9

--- boot.php
+++ boot.php
@@ -7,6 +7,12 @@ if (rex_addon::get('yform')->isAvailable()) {
 
 $addon = rex_addon::get('bs5_iconpicker');
 rex_view::addCssFile($addon->getAssetsUrl('/font/bootstrap-icons.css'));
-rex_view::addCssFile($addon->getAssetsUrl('/styles/bs5-iconpicker.css'));
-rex_view::addJsFile($addon->getAssetsUrl('js/iconpicker.js'));
+
+rex_view::addCssFile($addon->getAssetsUrl('/styles/bs5.css'));
+rex_view::addCssFile($addon->getAssetsUrl('/styles/fa6.css'));
+
 rex_view::addJsFile($addon->getAssetsUrl('js/bs5-iconpicker.js'));
+rex_view::addJsFile($addon->getAssetsUrl('js/fa6-iconpicker.js'));
+
+rex_view::addJsFile($addon->getAssetsUrl('js/bs5-picker.js'));
+rex_view::addJsFile($addon->getAssetsUrl('js/fa6-picker.js'));
